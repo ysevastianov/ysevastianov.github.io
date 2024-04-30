b095d890d60bbb40b37d00dbb21b60b426dd1142
Yevhen Sevastianov <63124965+ysevastianov@users.noreply.github.com>
2024-04-30 15:22:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('posts/posts.json')
         .then(response => response.json())
         .then(posts => {
+            // Sort posts in descending order based on filenames
+            posts = posts.sort((a, b) => {
+                const dateA = new Date(a.replace('.html', ''));
+                const dateB = new Date(b.replace('.html', ''));
+                return dateB - dateA;
+            });
+
             const container = document.getElementById('posts-container');
             posts.forEach(post => {
-                fetch(`posts/${post}`) // Fetch each post content
-                    .then(response => response.text()) // Get the response text
+                fetch(`posts/${post}`)
+                    .then(response => response.text())
                     .then(htmlContent => {
-                        const postContainer = document.createElement('div'); // Create a div to hold the post
+                        const postContainer = document.createElement('div');
                         postContainer.className = 'post';
-                        postContainer.innerHTML = htmlContent; // Insert the HTML content of the post
-                        container.appendChild(postContainer); // Append the post to the container
+                        postContainer.innerHTML = htmlContent;
+                        container.appendChild(postContainer);
                     })
                     .catch(err => console.error('Failed to load post content:', err));
             });
