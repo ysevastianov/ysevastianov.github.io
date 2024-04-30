2a8ee39d710ab6caf163ef86f7b400abc17313ed
Yevhen Sevastianov <63124965+ysevastianov@users.noreply.github.com>
2024-04-30 11:53:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // List your post files here
     const posts = [
-        'posts/2024-04-30-first-post.md',
-        'posts/2024-05-01-another-post.md'
+        'posts/test.html'
     ];
 
     posts.forEach(function(post) {
         fetch(post)
             .then(response => response.text())
-            .then(text => {
-                const html = marked(text);
+            .then(html => {
                 postsContainer.innerHTML += `<div class="post">${html}</div>`;
             })
             .catch(err => console.error('Error loading the post:', err));
