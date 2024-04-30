75bbc435b6b5ad4748ac26af316ba6c027c24b44
Yevhen Sevastianov <63124965+ysevastianov@users.noreply.github.com>
2024-04-30 16:43:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('posts/posts.json')
-        .then(response => response.json())
-        .then(posts => {
-            // Sort posts in descending order based on filenames
-            posts = posts.sort((a, b) => {
-                const dateA = new Date(a.replace('.html', ''));
-                const dateB = new Date(b.replace('.html', ''));
-                return dateB - dateA;
-            });
+document.addEventListener('DOMContentLoaded', async function() {
+    try {
+        const response = await fetch('posts/posts.json');
+        const posts = await response.json();
+        
+        // Sort posts in descending order based on filenames
+        posts.sort((a, b) => {
+            const dateA = new Date(a.replace('.html', ''));
+            const dateB = new Date(b.replace('.html', ''));
+            return dateB - dateA;
+        });
 
-            const container = document.getElementById('posts-container');
-            posts.forEach(post => {
-                fetch(`posts/${post}`)
-                    .then(response => response.text())
-                    .then(htmlContent => {
-                        const postContainer = document.createElement('div');
-                        postContainer.className = 'post';
-                        postContainer.innerHTML = htmlContent;
-                        container.appendChild(postContainer);
-                    })
-                    .catch(err => console.error('Failed to load post content:', err));
-            });
-        })
-        .catch(err => console.error('Failed to load posts:', err));
+        const container = document.getElementById('posts-container');
+        for (const post of posts) {
+            const response = await fetch(`posts/${post}`);
+            const htmlContent = await response.text();
+
+            const postContainer = document.createElement('div');
+            postContainer.className = 'post';
+            postContainer.innerHTML = htmlContent;
+            container.appendChild(postContainer);
+        }
+    } catch (err) {
+        console.error('Failed to load posts:', err);
+    }
 });
