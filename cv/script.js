d1feed1b8cfeebc6e7fb27816677b694386a840b
Yevhen Sevastianov <63124965+ysevastianov@users.noreply.github.com>
2024-05-01 14:00:55 +0300

    Create script.js

---
+++ cv/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setInterval(updateMemory, 5000);
+});
+
+function updateMemory() {
+    const memUsed = Math.floor(Math.random() * 300000) + 700000;
+    const memFree = 1000000 - memUsed;
+    const memOutput = `KiB Mem : 1000000 total, ${memFree} free, ${memUsed} used, 200000 buff/cache`;
+
+    const preElement = document.getElementById('top-output');
+    const lines = preElement.textContent.split('\n');
+    lines[2] = memOutput;
+    preElement.textContent = lines.join('\n');
+}
